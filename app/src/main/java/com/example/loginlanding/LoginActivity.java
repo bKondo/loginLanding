eaa5f3c1338b174ad0434429adf69a4ffdc6fc8d
Barbara Kondo <bkondo@csumb.edu>
2021-09-03 11:47:12 -0700

    changed compile sdk and target sdk to 28 in build.gradle

--- app/src/main/java/com/example/loginlanding/LoginActivity.java
+++ app/src/main/java/com/example/loginlanding/LoginActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import android.util.Log;
 
+public class LoginActivity extends AppCompatActivity {
+    public static final String ACTIVITY_LABEL = "LOGIN_ACTIVITY";
+
+    private ArrayList<String> usernames = new ArrayList<>();
+
+    private ArrayList<String> passwords = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        usernames.add("Bret");
+        usernames.add("Antonette");
+        usernames.add("Samantha");
+        usernames.add("Karianne");
+        usernames.add("Kamren");
+        usernames.add("Leopoldo");
+        usernames.add("Elwyn");
+        usernames.add("Maxime");
+        usernames.add("Delphine");
+        usernames.add("Moriah");
+
+        Log.d(ACTIVITY_LABEL, "usernames: " + usernames);
+
+        for (int i = 1; i < 11; i++) {
+            passwords.add(String.format("pass%d",i));
+        }
+
+        Log.d(ACTIVITY_LABEL, "passwords: " + passwords);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
